cf28cb8e744601cf7f88131b3e719ce8727c1c52
SaschaKohler <sascha.kohler@sent.at>
2021-07-22 15:28:35 +0200

    fun too

---
+++ kom_jquery.js
@@ -0,0 +1,16 @@
+
+
+  $('button').click(function () {
+    // Excercise 2
+    // $("input[name!='color']").siblings().append(' goon');
+
+    // Excercise 3
+    //$("input[name|='p']").attr('value','spass');
+
+    //Excercise 4
+    //$('p').before().prepend('fun ');
+
+    //Excercise 5
+    $('<div>Total Fun </div>').insertBefore('p');
+
+  });
