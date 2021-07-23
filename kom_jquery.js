cc276d9dfeaf06d5523762bc513ce0cc7bf6a524
SaschaKohler <sascha.kohler@sent.at>
2021-07-23 11:30:58 +0200

    Until 8 ready

--- kom_jquery.js
+++ kom_jquery.js
@@ -1,6 +1,6 @@
 
 
-  $('button').click(function () {
+  $(document).ready(function () {
     // Excercise 2
     // $("input[name!='color']").siblings().append(' goon');
 
@@ -11,6 +11,38 @@
     //$('p').before().prepend('fun ');
 
     //Excercise 5
-    $('<div>Total Fun </div>').insertBefore('p');
+    // $('<div>Total Fun </div>').insertBefore('p');
+
+    // Excercise 6
+    // $("div").children().css('background-color','yellow');
+
+    //Excercise 7
+
+    // $('<p id="fun">').insertBefore('input');
+    // $('input').css('padding','1em');
+    // $('#fun').css({'padding' : '1em','background-color': 'red',
+    //  'box-shadow' : '2px 2px 20px red'});
+    //
+    // $('input').blur(function () {
+    //   $('#fun').text('You blurred me!');
+    //   $('#fun').slideUp();
+    //
+    // })
+    // $('input').focus(function(){
+    //   $('#fun').slideDown();
+    //
+    // })
+
+    // Excercise 8
+
+    // $('div').click( function(){
+    //   let color = $(this).attr('style').split(':');
+    //   $('p').text('That div is ' + color[1]);
+    //   $('p').fadeIn(200);
+    //   $('p').fadeOut(1000);
+    //
+    // })
+
+    
 
   });
